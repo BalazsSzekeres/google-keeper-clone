e13dfa491f4b6cf6209aa3390c68e95331e31b83
Balazs Szekeres <balazs@bspace.nl>
2021-02-28 12:49:59 +0100

    Added basic layout and formatting

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+
+function Footer() {
+    return <footer><p>This is a sample app showing off React.JS</p></footer> ;
+}
+
+export default Footer;
