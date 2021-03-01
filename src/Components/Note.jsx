cc1e38cbf91353a451a3acf31e1133925b9abcde
Balazs Szekeres <balazs.szekeres.eu@gmail.com>
2021-03-01 15:53:55 +0100

    Added note component prop

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
 
-function Note() {
+function Note(props) {
     return <div className="note">
-        <h1>Sample Title</h1>
-        <p>Sample paragraph</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div> ;
 }
 
+
+
+
 export default Note
+
